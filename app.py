2835e056b64b86339f2dfe341cc0f0ceb11d7779
Ilya Mozerov <ilyailiilia@gmail.com>
2017-01-24 00:13:36 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+app = Flask(__name__)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
