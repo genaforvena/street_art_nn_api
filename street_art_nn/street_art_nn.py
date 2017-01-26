504c6055786b89b1b650df8b03c838bf00f1ab90
Ilya Mozerov <ilyailiilia@gmail.com>
2017-01-27 00:31:50 +0300

    Added pages to login and show entries

--- street_art_nn/street_art_nn.py
+++ street_art_nn/street_art_nn.py
@@ -2,7 +2,8 @@
 import json
 import sqlite3
 import os
-from flask import g, Flask, jsonify
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
 DATABASE = 'art_nn.db'
 
@@ -89,9 +90,34 @@ def get_tasks():
     return jsonify({'artworks': artworks_json})
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    db = get_db()
+    cur = db.execute('select title, artist, image from art order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
