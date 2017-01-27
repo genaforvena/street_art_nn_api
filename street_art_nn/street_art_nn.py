e3aa4da93f953f68ee590f415232873d39016165
Ilya Mozerov <ilyailiilia@gmail.com>
2017-01-27 22:44:10 +0300

    Added delete option to html and server

--- street_art_nn/street_art_nn.py
+++ street_art_nn/street_art_nn.py
@@ -74,6 +74,7 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
@@ -119,10 +120,20 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 
+@app.route('/delete', methods=['POST'])
+def delete_entry():
+    if not session.get('logged_in'):
+        abort('401')
+    db = get_db()
+    db.execute('DELETE FROM art WHERE id=?', [request.form["id"]])
+    db.commit()
+    return redirect(url_for('show_entries'))
+
+
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, artist, image, address from art order by id desc')
+    cur = db.execute('select id, title, artist, image, address from art order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
